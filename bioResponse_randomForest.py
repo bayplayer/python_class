21e1f9f571b6125d110413877a2efe092aba7c45
Timothy Liu <timothyliu@Timothys-MacBook-Pro.local>
2015-07-29 23:12:55 -0700

    add bioResponse

---
+++ bioResponse_randomForest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from sklearn.ensemble import RandomForestClassifier
+import csv_io
+
+def main():
+    train = csv_io.read_data("Data/train.csv")
+    target = [x[0] for x in train]
+    train = [x[1:] for x in train]
+    test = csv_io.read_data("Data/test.csv")
+
+    rf = RandomForestClassifier(n_estimators=100)
+    rf.fit(train, target)
+    predicted_probs = rf.predict_proba(test)
+    predicted_probs = ["%f" % x[1] for x in predicted_probs]
+    csv_io.write_delimited_file("Data/rf_benchmark.csv",
+                                predicted_probs)
+
+if __name__=="__main__":
+    main()
